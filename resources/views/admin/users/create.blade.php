147375c82a7f51d0731bd0dfc38ce670b367eaf1
Reza <reza@gmail.com>
2018-06-07 14:15:56 +0430

    8th commit - able to create post with static category

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -6,7 +6,7 @@
 
     {!! Form::open(['method'=>'POST', 'action'=> 'AdminUsersController@store','files'=>true]) !!}
         <div class="form-group">
-            {!! Form::label('name', 'Name:') !!}
+            {!! Form::label('name', 'Name') !!}
             {!! Form::text('name', null, ['class'=>'form-control'])!!}
         </div>
         <div class="form-group">
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,5 @@
-<h1>404</h1>
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-center">Oops the page that you're looking for doe's not exist <br><strong>ERROR 404</strong></h1>
+@endsection
